64f584458ef71aba34ae942bbd420193f7c2bd45
Oliver Crawford <oicrawford@gmail.com>
2024-02-22 17:28:41 -0600

    Add project files.

---
+++ OOP_week_6_assignment/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OOP_week_6_assignment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
